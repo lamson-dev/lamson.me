1b0311e5071a59f6c0402fda3c1881c2b1ed978f
Son Nguyen <son@lamson.me>
2012-12-21 03:58:28 -0800

    modified index files with php code

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+		<div id="footer">
+			<div id="copyright">&copy Son Nguyen, 2012. All Rights Reserved.
+				
+			</div>
+				
+		</div>
---
+++ includes/header.php
@@ -0,0 +1,19 @@
+	<base href="http://lamson.me" />
+	<meta http-equiv="Content-type" content="text/html;charset=utf-8" />
+
+	<!-- customized -->
+	
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="icon" type="image/png" href="images/favicon.png" />
+	<!--<link type="image/png" sizes="16x16" href="https://static.myopera.com/avatars/pool1/d8/0db/3b8da93f607119bee94afcb5da7e2ba9c7d.png" rel="icon">-->
+
+	<link href='http://fonts.googleapis.com/css?family=Gochi+Hand' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Permanent+Marker' rel='stylesheet' type='text/css'>
+
+
+	<link rel="stylesheet" type="text/css" media="all" href="http://projects.lamson.me/klphotos/templates/theworldin35mm/themes/light.css" />
+	<!--[if lt IE 9]>
+	<link rel="stylesheet" type="text/css" media="all" href="templates/theworldin35mm/css/theworldin35mm-ie.css" charset="utf-8" />
+	<![endif]-->
+
+
